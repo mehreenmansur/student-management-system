d0b7dca01e04749d30cf25d753dc0301b7271322
Mehreen Mansur <mehreen.mansur@gmail.com>
2017-05-18 11:45:14 +0600

    added deploy_to path

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/mehreen/projects/student_management_system"
+set :deploy_to,       "/home/mehreen/apps/sms"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
